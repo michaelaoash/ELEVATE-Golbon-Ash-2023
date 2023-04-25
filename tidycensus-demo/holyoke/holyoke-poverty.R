5b8be0971ce26f7a632e5ce1386e2cc6870dde5b
Michael Ash <michaelaoash@gmail.com>
2023-04-25 00:15:09 -0400

    Some adjustments to the eGRID module

--- tidycensus-demo/holyoke/holyoke-poverty.R
+++ tidycensus-demo/holyoke/holyoke-poverty.R
@@ -1,4 +1,4 @@
-pdf(file="holyoke-poverty.pdf",height=0,width=0, paper="USr")
+## pdf(file="holyoke-poverty.pdf",height=0,width=0, paper="USr")
 ## https://www.holyoke.org/maps-of-holyoke/
 ## https://en.wikipedia.org/wiki/Template:Holyoke,_Massachusetts_Labelled_Map
 library(tidyverse)
@@ -10,6 +10,7 @@ library(sp)
 library(sf)
 options(width=200)
 ## Documentation https://walker-data.com/tidycensus/articles/basic-usage.html
+## Get your own Census API Key
 ## https://api.census.gov/data/key_signup.html
 census_api_key("cb9bd8756de7ba64b3c95ec0bd9193fc98d7cfe1")
 
@@ -30,10 +31,12 @@ acs_v2020 %>% filter(grepl("B17010._(004)",name))
 ## "B17010_037","Estimate!!Total:!!Income in the past 12 months at or above poverty level:!!Other family:!!Female householder, no spouse present:!!With related children of the householder under 18 years:"
 
 
+## Use these to get some useful geography
 ma_towns_acs2020 <- get_acs(year=2020, geography = "county subdivision", state="MA", keep_geo_vars=TRUE, geometry = TRUE, output="wide",
                             variables = "B01001_001")
 holyoke_acs2020 <- ma_towns_acs2020 %>% filter(NAME.x=="Holyoke")
 
+## Download blockgroup-level family poverty counts for all of Massachusetts
 ma_blockgroups_acs2020 <- get_acs(year=2020, geography = "block group", state="MA", keep_geo_vars=TRUE, geometry = TRUE, output="wide",
                                   variables = c(
                                       "B17010_004",
@@ -45,24 +48,26 @@ ma_blockgroups_acs2020 <- get_acs(year=2020, geography = "block group", state="M
                                   )
                                   )
 
+## Compute the poverty rate from the count data
 ma_blockgroups_acs2020 <- ma_blockgroups_acs2020 %>% mutate(
                                                          poor_families_with_children = B17010_004E + B17010_011E + B17010_017E,
                                                          families_with_children = B17010_004E + B17010_011E + B17010_017E + B17010_024E + B17010_031E + B17010_037E,
                                                          percent_families_with_children_in_poverty = poor_families_with_children/families_with_children * 100
                                                          )
 
+## Plot some maps
 ggplot(ma_blockgroups_acs2020) + geom_sf()
 
 hampden_blockgroups_acs2020 <- filter(ma_blockgroups_acs2020, COUNTYFP=="013")
 
 ggplot(hampden_blockgroups_acs2020) + geom_sf()
 
+## As it happens, Tracts can neatly distinguish Holyoke from surrounding cities and towns.
 holyoke_blockgroups_acs2020 <- filter(hampden_blockgroups_acs2020, substr(TRACTCE,1,4) %in% c("8114","8115","8116","8117","8118","8119","8120","8121"))
 
 ggplot(holyoke_blockgroups_acs2020) + geom_sf() + geom_sf_text(aes(label=GEOID))
 
 ggplot(holyoke_blockgroups_acs2020) + geom_sf(aes(fill=percent_families_with_children_in_poverty))
-
 
 
 ## Source: https://www2.census.gov/geo/tiger/TIGER2022/
